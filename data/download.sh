4e4166cf53f555fcb1b5b6fa62c15651bad1ee79
limsan <ms.sandar.lim@gmail.com>
2018-05-21 01:39:08 +0200

    Stanford sentiment tree

--- data/download.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-curl https://polybox.ethz.ch/index.php/s/huqymt5WErXxpuW/download > cloze_test_val.csv
-curl https://polybox.ethz.ch/index.php/s/l2wM4RIyI3pD7Tl/download > train_stories.csv
