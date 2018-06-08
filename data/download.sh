908df1ae23f56d6a83c8bcdaac40133118aee4b3
Stefano Woerner <mail@stefano-woerner.de>
2018-06-08 22:16:33 +0200

    fix downloader

--- data/download.sh
+++ data/download.sh
@@ -4,5 +4,5 @@ curl https://polybox.ethz.ch/index.php/s/l2wM4RIyI3pD7Tl/download > train_storie
 curl https://polybox.ethz.ch/index.php/s/AKbA8g7SeHwjU0R/download > test_nlu18.csv
 curl https://polybox.ethz.ch/index.php/s/h2gp3FpS3N7Xgiq/download > cloze_test_spring2016-test.csv
 mkdir -p glove.6B
-curl https://polybox.ethz.ch/index.php/s/xMEpgB31AfR1q1k/download > glove.6B/glove.6B.100d.word2vec.txt
+curl https://polybox.ethz.ch/index.php/s/IpNCtbfvc1H3kZg/download > glove.6B/glove.6B.100d.word2vec.txt
 
